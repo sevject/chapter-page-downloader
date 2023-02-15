a6f4700219f4dead68af2d95ef4d9228ad5cb815
Tina Hammer <sevject@users.noreply.github.com>
2023-02-15 21:16:33 +0100

    Create pageDownload.sh

---
+++ pageDownload.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+############################################################
+# Default values                                           #
+############################################################
+
+link=""
+imgtype="png"
+imgdir="."
+
+############################################################
+# Help                                                     #
+############################################################
+Help()
+{
+   # Display Help
+   echo "This script downloads images from the given url"
+   echo
+   echo "Syntax: pageDownload.sh [-h] [url] [chapter] [first] [last] [type]"
+   echo "params:"
+   echo "url        url, ie. http://example.com/"
+   echo "chapter    chapter, ie. 0237, default 0001"
+   echo "first      [optional] starting page, ie. 1, default 1"
+   echo "last       [optional] last page, ie. 15, default 10"
+   echo "type       [optional] image type, ie. jpeg, default png"
+   echo "options:"
+   echo "h          Print this Help."
+   echo
+}
+
+while getopts ":h" option; do
+   case $option in
+      h) # display Help
+         Help
+         exit;;
+      \?) # incorrect option
+         echo "Error: Invalid option"
+         exit;;
+   esac
+done
+
+############################################################
+# Main program                                             #
+############################################################
+
+url=$1
+chapter=$2
+start=$3
+end=$4
+type=$5
+
+if [ -z "$url" ]; then
+    url=$link
+fi
+
+if [ -z "$chapter" ]; then
+    chapter="0001"
+fi
+
+if [ -z "$start" ]; then
+    start=1
+fi
+
+if [ -z "$end" ]; then
+    end=10
+fi
+
+if [ -z "$type" ]; then
+    type=$imgtype
+fi
+
+# todo
+
+echo "Done."
+
+############################################################
+# Copyright: T. H.                                         #
+# https://github.com/sevject/chapter-page-downloader       #
+# License: GPLv3                                           #
+############################################################
